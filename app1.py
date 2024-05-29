86b0daac54cd9d03a63fe38068448fafa99ad70c
Priya5MC <134297065+Priya5MC@users.noreply.github.com>
2024-05-29 16:41:34 +0530

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,31 +1,29 @@
-import streamlit as st
+import numpy as np
 import pandas as pd
+import streamlit as st
+import joblib
+import nvdlib
+
 
 def main():
-    st.title('Field Value Details')
-
-    # Sample data
-    data = {
-        'Name': ['John', 'Jane', 'Alice'],
-        'Age': [30, 25, 35],
-        'Occupation': ['Engineer', 'Designer', 'Doctor']
-    }
-
-    df = pd.DataFrame(data)
-
-    st.write('## Data Table')
-    st.dataframe(df)
-
-    # Create a selectbox to choose a name
-    selected_name = st.selectbox('Select a Name to see details', df['Name'])
-
-    if selected_name:
-        # Get the details of the selected name
-        selected_row = df[df['Name'] == selected_name].iloc[0]
+    st.title('NVD')
+    html_tmp = """
+    <div style='background-color:red;'>
+    <h2 style='color:white;text-align:center;'>CVE LIST</h2>
+    </div>
+    """
+    data = selected_df
+    st.markdown(html_tmp, unsafe_allow_html=True)
+    st.write("First five rows")
+    st.dataframe(data.head(5))
+    selected_index = st.selectbox('Select a row index to see details', data.index)
+    if selected_index is not None:
+        # Get the details of the selected row
+        selected_row = data.iloc[selected_index]
         st.write('## Additional Details')
-        st.write(f'**Name:** {selected_row["Name"]}')
-        st.write(f'**Age:** {selected_row["Age"]}')
-        st.write(f'**Occupation:** {selected_row["Occupation"]}')
+    
+       
+         
 
-if __name__ == '__main__':
-    main()
+if __name__=='__main__':
+        main()
