296a7eed1a6c78f07c35deabba04c1c5140da19b
Priya5MC <134297065+Priya5MC@users.noreply.github.com>
2024-05-29 20:37:36 +0530

    Add files via upload

--- app1.py
+++ app1.py
@@ -13,7 +13,7 @@ def main():
     data = pd.read_csv('NVD.csv')
     st.markdown(html_tmp, unsafe_allow_html=True)
     st.write("First five rows")
-    st.dataframe(data.head(5))
+    st.dataframe(data)
 
  
     selected_index = st.selectbox('Select a row index to see details', data.index)
@@ -21,7 +21,21 @@ def main():
         # Get the details of the selected row
         selected_row = data.iloc[selected_index]
         st.write('## Additional Details')
-    
+
+        data = {
+    "ID": [1, 2, 3],
+    "Names": [["Alice", "Bob"], ["Charlie", "David"], ["Eve", "Frank"]]
+      }
+
+# Configure the list column
+    list_column_config = st.column_config.ListColumn(
+        label="Names",
+        help="List of names associated with each ID"
+     )
+
+# Display the dataframe with the configured list column
+    st.dataframe(data, column_config={"Names": list_column_config})  
+ 
        
          
 
